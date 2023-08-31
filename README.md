8b039e087478dbbe5d7bfa97c6d0d27e3dbda144
Scott J Maddox <scottjmaddox@gmail.com>
2023-08-31 01:26:03 -0500

    WIP add sqlx

--- README.md
+++ README.md
@@ -4,8 +4,42 @@ Work in progress boilerplate for a website with Rust/Axum/SQLx backend and HTMX 
 
 ## Table of Contents
 
+- [Running](#running)
+- [Developing](#developing)
+    - [Adding Migrations](#adding-migrations)
 - [Contributing](#contributing)
 - [License](#license)
+
+## Running
+
+1. Declare the database URL:
+
+    ```
+    export DATABASE_URL="sqlite:db.sqlite"
+    ```
+
+1. Run the backend:
+
+    ```
+    cargo run
+    ```
+
+## Developing
+
+### Adding Migrations
+
+1. Install the SQLx command line tool:
+
+    ```
+    cargo install sqlx-cli
+    ```
+
+1. Use it to create a new migration script:
+
+    ```
+    cd backend
+    sqlx migrate add -r <DESCRIPTION>
+    ```
 
 ## Contributing
 
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,60 @@
 use axum::{response::Html, routing::get, Router};
+use color_eyre::{eyre, eyre::WrapErr};
+use sqlx::migrate::MigrateDatabase;
+use sqlx::sqlite::SqlitePool;
 use std::net::SocketAddr;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() {
+    color_eyre::install().unwrap();
+    run().await.unwrap();
+}
+
+async fn run() -> eyre::Result<()> {
+    let pool = create_db().await?;
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+    assert_eq!(row.0, 150);
+
     let app = Router::new().route("/", get(handler));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+    Ok(())
+}
+
+async fn create_db() -> eyre::Result<SqlitePool> {
+    // Create the database
+    let db_url = std::env::var("DATABASE_URL")
+        .wrap_err("DATABASE_URL env var not set")?;
+
+    if !sqlx::Sqlite::database_exists(&db_url).await? {
+        sqlx::Sqlite::create_database(&db_url).await?;
+    }
+
+    // Connect to the database
+    let pool = SqlitePool::connect(&db_url).await?;
+
+    // Migrate the database
+    let migrations =
+        if std::env::var("RUST_ENV") == Ok("production".to_string()) {
+            std::env::current_exe()?.join("./migrations")
+        } else {
+            let crate_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+            Path::new(&crate_dir).join("./migrations")
+        };
+
+    sqlx::migrate::Migrator::new(migrations)
+        .await?
+        .run(&pool)
+        .await?;
+
+    Ok(pool)
 }
 
 async fn handler() -> Html<&'static str> {
